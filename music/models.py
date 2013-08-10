59bca00ae92f56589401968f9735071f0a72c1ca
Sean <berg@Seans-MacBook-Air.local>
2013-08-10 17:58:24 -0400

    added autogenerated models to music

--- music/models.py
+++ music/models.py
@@ -1,3 +1,23 @@
+from __future__ import unicode_literals
 from django.db import models
 
-# Create your models here.
+class Albums(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.TextField() # This field type is a guess.
+    artist_id = models.IntegerField()
+    class Meta:
+        db_table = 'albums'
+
+class Artists(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.TextField(unique=True) # This field type is a guess.
+    class Meta:
+        db_table = 'artists'
+
+class Tracks(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.TextField() # This field type is a guess.
+    album_id = models.IntegerField()
+    length = models.IntegerField()
+    class Meta:
+        db_table = 'tracks'
