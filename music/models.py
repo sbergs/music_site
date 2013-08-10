89827be88ec36eda1cdf623c81f10cd23277c03d
Sean <berg@Seans-MacBook-Air.local>
2013-08-10 18:06:29 -0400

    added foreign keys back in

--- music/models.py
+++ music/models.py
@@ -1,23 +1,23 @@
 from __future__ import unicode_literals
 from django.db import models
 
-class Albums(models.Model):
-    id = models.IntegerField(primary_key=True)
-    name = models.TextField() # This field type is a guess.
-    artist_id = models.IntegerField()
-    class Meta:
-        db_table = 'albums'
-
 class Artists(models.Model):
     id = models.IntegerField(primary_key=True)
-    name = models.TextField(unique=True) # This field type is a guess.
+    name = models.CharField(max_length=255,unique=True) # This field type is a guess.
     class Meta:
         db_table = 'artists'
 
+class Albums(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=255) # This field type is a guess.
+    artist_id = models.ForeignKey(Artists)
+    class Meta:
+        db_table = 'albums'
+
 class Tracks(models.Model):
     id = models.IntegerField(primary_key=True)
-    name = models.TextField() # This field type is a guess.
-    album_id = models.IntegerField()
+    name = models.CharField(max_length=255) # This field type is a guess.
+    album_id = models.ForeignKey(Albums)
     length = models.IntegerField()
     class Meta:
         db_table = 'tracks'
